9606262b0d90b8ca72b11bbf79dee62e56736b7c
Pablo Rodrigo Lima dos Santos <pablo.santos@mercadolivre.com>
2021-07-15 09:31:11 -0300

    sync: first commit, sync local repository with remote repository

---
+++ src/main/java/br/com/mercadolivre/seuimovel/SeuimovelApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.seuimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuimovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuimovelApplication.class, args);
+	}
+
+}
